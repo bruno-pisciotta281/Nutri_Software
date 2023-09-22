977c7332a1c35f53e779a8c92e30af109874b571
bruno-pisciotta281 <bruno.pisciotta43@gmail.com>
2023-09-22 11:20:35 -0300

    Adição da Funcionalidade de Ver Resolução na Calculadora de Get para Gestantes

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function calculateEER() {
 
   let EER = 0;
 
-  // Calcula o EER com base no nível de atividade
+  // Calcula o GET com base no nível de atividade
   switch (activityLevel) {
     case 'Sedentária':
       EER = 1131.20 - (2.04 * age) + (0.34 * heightInCm) + (12.15 * weight) + (9.16 * weeks);
@@ -75,4 +75,72 @@ function calculateNutrientRequirements() {
   // Atualize o resultado na página
   document.getElementById("result").innerText = `A quantidade de nutrientes necessária é ${nutrientRequirements.toFixed(2)} kcal/dia.`;
 }
+
+// Função para mostrar a resolução do cálculo de GET em um modal
+function showResolution() {
+  // Pegue as variáveis do formulário
+  const age = parseFloat(document.getElementById("age").value);
+  const heightInCm = parseFloat(document.getElementById("height").value);
+  const weight = parseFloat(document.getElementById("postWeight").value); // Peso pós-gestação agora
+  const weeks = parseFloat(document.getElementById("weeks").value);
+  const activityLevel = document.getElementById("activityLevel").value;
+
+  let EER = 0;
+  let explanation = ""; // Inicialize a explicação vazia
+
+  // Calcula o GET com base no nível de atividade e crie a explicação
+  switch (activityLevel) {
+    case 'Sedentária':
+      EER = 1131.20 - (2.04 * age) + (0.34 * heightInCm) + (12.15 * weight) + (9.16 * weeks);
+      explanation = `
+        A fórmula utilizada para calcular o GET no nível <strong>'Sedentária'</strong> é:<br><br>
+        <strong>EER =</strong> 1131.20 - (2.04 * idade) + (0.34 * altura em cm) + (12.15 * peso) + (9.16 * semanas de gestação).<br><br>
+        <strong>EER =</strong> 1131.20 - (2.04 * ${age}) + (0.34 * ${heightInCm}) + (12.15 * ${weight}) + (9.16 * ${weeks}).<br><br>
+        <strong>EER = ${EER.toFixed(2)} kcal/dia.</strong>
+      `;
+      break;
+    case 'Pouco Ativa':
+      EER = 693.35 - (2.04 * age) + (5.73 * heightInCm) + (10.20 * weight) + (9.16 * weeks);
+      explanation = `
+        A fórmula utilizada para calcular o GET no nível <strong>'Pouco Ativa'</strong> é:<br><br>
+        <strong>EER =</strong> 693.35 - (2.04 * idade) + (5.73 * altura em cm) + (10.20 * peso) + (9.16 * semanas de gestação).<br><br>
+        <strong>EER =</strong> 693.35 - (2.04 * ${age}) + (5.73 * ${heightInCm}) + (10.20 * ${weight}) + (9.16 * ${weeks}).<br><br>
+        <strong>EER =${EER.toFixed(2)} kcal/dia.</strong>
+      `;
+      break;
+    case 'Ativa':
+      EER = 223.84 - (2.04 * age) + (13.23 * heightInCm) + (8.15 * weight) + (9.16 * weeks);
+      explanation = `
+        A fórmula utilizada para calcular o GET no nível <strong>'Ativa'</strong> é:<br><br>
+        <strong>EER =</strong> 223.84 - (2.04 * idade) + (13.23 * altura em cm) + (8.15 * peso) + (9.16 * semanas de gestação).<br><br>
+        <strong>EER =</strong> 223.84 - (2.04 * ${age}) + (13.23 * ${heightInCm}) + (8.15 * ${weight}) + (9.16 * ${weeks}).<br><br>
+        <strong>EER =${EER.toFixed(2)} kcal/dia.</strong>
+      `;
+      break;
+    case 'Muito Ativa':
+      EER = 779.72 - (2.04 * age) + (18.45 * heightInCm) + (8.73 * weight) + (9.16 * weeks);
+      explanation = `
+        A fórmula utilizada para calcular o GET no nível <strong>'Muito Ativa'</strong> é:<br><br>
+        <strong>EER =</strong> 779.72 - (2.04 * idade) + (18.45 * altura em cm) + (8.73 * peso) + (9.16 * semanas de gestação).<br><br>
+        <strong>EER =</strong> 779.72 - (2.04 * ${age}) + (18.45 * ${heightInCm}) + (8.73 * ${weight}) + (9.16 * ${weeks}).<br><br>
+        <strong>EER = ${EER.toFixed(2)} kcal/dia. </strong>
+      `;
+      break;
+  }
+
+  // Preencha o conteúdo do modal com a resolução e explicação
+  const resolutionContent = document.getElementById("resolutionContent");
+  resolutionContent.innerHTML = `
+    <p><strong>GET estimado:</strong> ${EER.toFixed(2)} kcal/dia</p>
+  `;
+
+  // Preencha a explicação no modal
+  const explanationElement = document.getElementById("explanation");
+  explanationElement.innerHTML = explanation;
+
+  // Exiba o modal com a resolução
+  const modal = document.getElementById("resolutionModal");
+  modal.style.display = "block";
+}
+
 
