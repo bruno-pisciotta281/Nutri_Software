593649a8151dd554daccec827d6e78f6a0e9bd71
bruno-pisciotta281 <bruno.pisciotta43@gmail.com>
2023-09-27 15:51:27 -0300

    Atualização da função de criar usuário

---
+++ api/config.php
@@ -0,0 +1,13 @@
+<?php
+$host = "ep-frosty-union-26727263.us-east-1.postgres.vercel-storage.com";
+$port = "5432";
+$dbname = "verceldb";
+$user = "default";
+$password = "UMFsvA7JpZy9";
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+}
+?>
---
+++ api/processar_cadastro.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once("config.php");
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
+} catch (PDOException $e) {
+    die("Erro na conexão com o banco de dados: " . $e->getMessage());
+}
+
+// Coletar dados do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = password_hash($_POST['senha'], PASSWORD_DEFAULT); // Hash da senha
+
+// Inserir dados na tabela 'usuarios'
+$sql = "INSERT INTO usuarios (nome, email, senha) VALUES (:nome, :email, :senha)";
+$stmt = $pdo->prepare($sql);
+
+$stmt->bindParam(':nome', $nome);
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':senha', $senha);
+
+if ($stmt->execute()) {
+    echo "Usuário cadastrado com sucesso!";
+    // Redirecionar para a página de login
+    header("Location: login.html");
+} else {
+    echo "Erro ao cadastrar o usuário.";
+}
+?>
